2e78b5306a8ef0a9ffaa1b1f518af6d356c7134e
Yu Kumazaki <yukumazaki@Yus-MacBook-Pro.local>
2025-05-03 22:11:58 +0900

    r

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -19,6 +19,7 @@ import {
 import Image from "next/image"
 import Link from "next/link"
 import { useState, useEffect } from "react"
+import React from "react";
 
 import { Button } from "@/components/ui/button"
 import { Card } from "@/components/ui/card"
